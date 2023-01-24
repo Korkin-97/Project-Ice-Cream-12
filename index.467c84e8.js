01e74715569fc7bbbb0b7ed6c43147d31063b2dc
Korkin-97 <113092991+Korkin-97@users.noreply.github.com>
2023-01-24 20:15:09 +0000

    Deploying to gh-pages from @ Korkin-97/Project-Ice-Cream-12@b9a9da62a955240ef9598f1a7cfb7e1c89098e6d 🚀

---
+++ index.467c84e8.js
@@ -0,0 +1,2 @@
+!function(){new Swiper(".mySwiper",{spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0}});!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),close1ModalBtn:document.querySelector("[data-modal-close1]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.close1ModalBtn.addEventListener("click",o)}()}();
+//# sourceMappingURL=index.467c84e8.js.map
