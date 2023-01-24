8e2f2fde2ade54b6b295f88e51abc872703d3514
Viktor Koriakin <vitya.korkin94@gmail.com>
2023-01-24 22:41:06 +0200

    add burger

--- src/index.js
+++ src/index.js
@@ -11,12 +11,16 @@ var swiper = new Swiper('.mySwiper', {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     close1ModalBtn: document.querySelector('[data-modal-close1]'),
+    close2ModalBtn: document.querySelector('[data-modal-close2]'),
+    close3ModalBtn: document.querySelector('[data-modal-close3]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.close1ModalBtn.addEventListener('click', toggleModal);
+  refs.close2ModalBtn.addEventListener('click', toggleModal);
+  refs.close3ModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
