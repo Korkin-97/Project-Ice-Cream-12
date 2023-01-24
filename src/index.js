dc8b0105f619d408b667dc8e230cb40f18361ac8
Vitalii Kutovenko <idorikk@gmail.com>
2023-01-24 22:13:46 +0200

    1fixed

--- src/index.js
+++ src/index.js
@@ -10,11 +10,13 @@ var swiper = new Swiper('.mySwiper', {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    close1ModalBtn: document.querySelector('[data-modal-close1]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.close1ModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
